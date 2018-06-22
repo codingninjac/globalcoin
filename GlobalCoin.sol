c334ab6909d467c855d5ae187dc7b731b036a21e
BraindeadDeveloper <rootkit@localhost.localdomain>
2018-06-22 10:27:05 +0100

    Added events

--- GlobalCoin.sol
+++ GlobalCoin.sol
@@ -2,7 +2,8 @@
 /// @author tokendev@pm.me
 /// @dev This is not ERC20 Compatible. Only supports CAD token at the moment. 
 /// (to do) Implement USD, RMB tokens; Supports upgrading using proxy pattern
-/// (to do) Add deposit function for CADSeedBalance
+/// The owner who can execute upgrades is a multi-sig wallet that represents 
+/// the company's governance structure.
 pragma solidity ^0.4.24;
 
 
@@ -66,29 +67,33 @@ contract GlobalCoinCAD {
     }
     
     /*
-    In the future there might be just one function
-    for interests management as well as unlocking funds
-    Current interest rate is at 2% every 98 days
+    Current interest rate is at 2% every 3 months
     Intests will go directly to CADPointsBalance
     However, only CADSeedBalance is eligible for interests
     */
     function claimInterests(address _memberAddr) public returns (bool success) {
        Member storage _member = MemberMapping[_memberAddr];
-       require(now > _member.refreshTime + 98 days);
+       require(now > _member.refreshTime + 92 days);
        require(_member.isMember == true);
        _member.refreshTime = uint64(now);
        uint256 newInterests = _member.CADSeedBalance.div(100).mul(2);
        _member.CADPointsBalance = _member.CADPointsBalance.add(newInterests);
+       emit InterestsClaimed(_memberAddr, newInterests, now);
        return true;
     }
     
+    /*
+    10% of CADSeedBalance will be unlocked each year after becoming a member
+    */
     function unlockFund(address _memberAddr) public returns (bool success) {
         Member storage _member = MemberMapping[_memberAddr];
         require(now > _member.lockTime + 365 days);
         require(_member.isMember == true);
         _member.lockTime  = uint64(now);
-        _member.CADSeedBalance = _member.CADSeedBalance.sub(280);
-        _member.CADPointsBalance = _member.CADPointsBalance.add(280);
+        uint256 unlockedFund = _member.CADSeedBalance.div(100).mul(10);
+        _member.CADSeedBalance = _member.CADSeedBalance.sub(unlockedFund);
+        _member.CADPointsBalance = _member.CADPointsBalance.add(unlockedFund);
+        emit FundUnlocked(_memberAddr, unlockedFund, now);
         return true;
     }
     
@@ -97,7 +102,7 @@ contract GlobalCoinCAD {
     Each member will get 2800 CAD tokens upon registeration 
     Only contract owner can add new members
     */
-    function approveMembership(address _newMember) public onlyOwner {
+    function approveMembership(address _newMember) public onlyOwner returns (bool success) {
         totalSupply = totalSupply.add(2800);
         Member storage _member = MemberMapping[_newMember];
         _member.isMember = true;
@@ -105,9 +110,9 @@ contract GlobalCoinCAD {
         _member.CADSeedBalance = 2520;
         _member.CADPointsBalance = 280;  // 10% of fund will be spendable immediatly
         emit MemberJoined(_newMember, now);
+        return true;
     }   
-  
-
+    
     /*
     Members can spend CADPointsBalance
     OR transfer CADPoints to each other
@@ -124,4 +129,6 @@ contract GlobalCoinCAD {
 
    event TransferPoints(address indexed _from, address indexed _to, uint256 _value);
    event MemberJoined(address indexed _newMember, uint256 _joinTime);
+   event InterestsClaimed(address indexed _memberAddr, uint256 _amount, uint256 _time);
+   event FundUnlocked(address indexed _memberAddr, uint256 _amount, uint256 _time);
 }
